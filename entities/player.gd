8ccae50ca7e9f4eb5801bc383c9c14268bdc3122
Aymeric Conti <aymeric.conti2002@gmail.com>
2025-04-21 10:23:59 +0200

    correct world borders

--- entities/player.gd
+++ entities/player.gd
@@ -5,9 +5,10 @@ class_name Player extends CharacterBody2D
 @export var rotation_speed: float = 2.0
 
 func _physics_process(delta: float) -> void:
-	position += speed * delta * Vector2.from_angle(rotation - PI / 2)
-
 	if Input.is_action_pressed("right"):
 		rotation += rotation_speed * delta
 	elif Input.is_action_pressed("left"):
 		rotation -= rotation_speed * delta
+
+	velocity = speed * Vector2.from_angle(rotation - PI / 2)
+	move_and_slide()
