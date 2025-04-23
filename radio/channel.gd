da21dcb720b592f3e981a45d1b3c68d6dd0ef7df
m.conti <conti.matthieu@gmail.com>
2025-04-24 02:17:59 +0800

    radio name

--- radio/channel.gd
+++ radio/channel.gd
@@ -1,4 +1,4 @@
 class_name RadioChannel extends Resource
 
-@export var sounds: Array[AudioStream] = []
+@export var sounds: Array[RadioSound] = []
 @export var display_name: String
---
+++ radio/sound.gd
@@ -0,0 +1,4 @@
+class_name RadioSound extends Resource
+
+@export var stream: AudioStream
+@export var display_name: String
