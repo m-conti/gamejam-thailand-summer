124d5cd8419d4f5aa5e8f8e792a2d3f905af8f6b
m.conti <conti.matthieu@gmail.com>
2025-04-24 17:20:47 +0800

    light

--- powers/slower.gd
+++ powers/slower.gd
@@ -2,10 +2,10 @@ class_name Slower extends Power
 
 
 func _enter_tree() -> void:
-    player.base_speed -= 100
-    player.max_speed -= 100
+	player.base_speed -= 100
+	player.max_speed -= 100
 
 
 func _exit_tree() -> void:
-    player.base_speed += 100
-    player.max_speed += 100
+	player.base_speed += 100
+	player.max_speed += 100
