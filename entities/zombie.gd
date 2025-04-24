b8cbbb9e8e82ace4fab1035f8e411d9fbf19dd4a
m.conti <conti.matthieu@gmail.com>
2025-04-24 14:16:05 +0800

    zombies

--- entities/zombie.gd
+++ entities/zombie.gd
@@ -1,1 +1,10 @@
 class_name Zombie extends Entity
+
+const resource = [
+	"res://assets/Player/Idle2.png",
+	"res://assets/Player/Idle3.png",
+	"res://assets/Player/Idle4.png",
+]
+
+func _ready():
+	%Sprite2D.texture = load(resource[randi_range(0, len(resource) - 1)])
